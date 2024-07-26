ab2b17a6b12f85156a4c81ceb706442b65b8d31a
sksksk2024 <157216629+sksksk2024@users.noreply.github.com>
2024-07-26 16:34:54 +0100

    Added the project, but the design of the phone is not complete, and likewise with the desktop

---
+++ app/js/script.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("button").addEventListener('click', function() {
+    document.querySelector(".share").style.display = 'flex';
+    document.getElementById("buttonClose").style.display = 'inline-block';
+    document.getElementById("button").style.display = 'none';
+  });
+
+  function init() {
+    document.querySelector(".share").style.display = 'none';
+    document.getElementById("button").style.display = 'inline-block';
+    document.getElementById("buttonClose").style.display = 'none';
+  };
+
+  document.getElementById("buttonClose").addEventListener('click', init);
+  document.getElementById("buttonCloseOne").addEventListener('click', init);
+});
+
+
+
+
+
+
+// document.getElementById("button").addEventListener('click', function(){
+//   document.querySelector(".share").computedStyleMap.display = 'flex';
+//   document.getElementById("buttonClose").style.display = 'inline-block';
+//   document.getElementById("button").style.display = 'none';
+// });
+
+// function init(){
+//   document.querySelector(".share").style.display = 'none';
+//   document.getElementById("button").style.display = 'inline-block';
+//   document.getElementById("buttonClose").style.display = 'none';
+// };
+
+// document.getElementById("buttonClose").addEventListener('click', init);
+// document.getElementById("buttonCloseOne").addEventListener('click', init);
---
+++ style-guide.md
@@ -0,0 +1,29 @@
+# Front-end Style Guide
+
+## Layout
+
+The designs were created to the following widths:
+
+- Mobile: 375px
+- Desktop: 1440px
+
+
+
+## Typography
+
+### Body Copy
+
+- Font size: 13px
+
+### Headings
+
+- Family: [Manrope](https://fonts.google.com/specimen/Manrope)
+- Weights: 500, 700
+
+## Icons
+
+We provide SVGs for the social icons. But please feel free to use a font icon library if you like. Some suggestions can be found below:
+
+- [Font Awesome](https://fontawesome.com)
+- [IcoMoon](https://icomoon.io)
+- [Ionicons](https://ionicons.com)
